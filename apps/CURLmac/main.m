857209131207654c30b37e4115a996ea685c0e46
Jeffrey Bergier <jeffburg@jeffburg.com>
2026-03-26 20:16:13 +0900

    [CURL] cleaned up makefiles

--- apps/CURLmac/main.m
+++ apps/CURLmac/main.m
@@ -1,6 +1,6 @@
 //
 //  main.m
-//  SingleWindow
+//  CURLmac
 //
 
 #import <AppKit/AppKit.h>
@@ -20,4 +20,11 @@ int main(int argc, char *argv[])
   // Close
   [pool release];
   return 0;
+}
+
+// --- Linker Stubs ---
+// This symbol is required by modern Clang for x64 linking when targeting 10.11+
+// in environments with legacy runtime libraries.
+int __isPlatformVersionAtLeast(int p, int maj, int min, int rev) {
+    return 1;
 }
