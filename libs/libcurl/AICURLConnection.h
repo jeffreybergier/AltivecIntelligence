a17a158a70fb54e83c56b7acc327bbf8343986be
Jeffrey Bergier <jeffburg@jeffburg.com>
2026-03-27 19:45:01 +0900

    [AICC] Fixed problem with CACert not being in app

--- libs/libcurl/AICURLConnection.h
+++ libs/libcurl/AICURLConnection.h
@@ -1,10 +1,14 @@
 #import <Foundation/Foundation.h>
 
-@interface AICURLConnection : NSObject
+@interface AICURLConnection: NSObject
 
 + (NSString *)curlVersion;
 + (NSString *)opensslVersion;
 + (NSString *)zlibVersion;
 + (NSString *)aicVersion;
++ (NSString *)certPath;
+
+- (void)__newCURLHandle:(void *)handle;
+- (void)__releaseCURLHandle:(void *)handle;
 
 @end
--- libs/libcurl/AICURLConnection.m
+++ libs/libcurl/AICURLConnection.m
@@ -4,7 +4,24 @@
 #import <openssl/crypto.h>
 #import <zlib.h>
 
+
 @implementation AICURLConnection
+
++ (void)initialize {
+  if (self == [AICURLConnection class]) {
+    // Check that ca certificates file can be found
+    [self certPath];
+    // Initialize libcurl globally
+    curl_global_init(CURL_GLOBAL_ALL);
+  }
+}
+
++ (NSString *)certPath;
+{
+  NSString *certPath = [[NSBundle mainBundle] pathForResource:@"cacert" ofType:@"pem"];
+  NSParameterAssert(certPath);
+  return certPath;
+}
 
 + (NSString *)curlVersion {
     const char *ver = curl_version();
@@ -24,6 +41,21 @@
 
 + (NSString *)aicVersion {
     return @"1.0.0-altivec";
+}
+
+- (void)__newCURLHandle:(void *)handle;
+{
+  CURL *curl = (CURL *)handle;
+  NSParameterAssert(curl);
+  NSString *certPath = [[self class] certPath];
+  curl_easy_setopt(curl, CURLOPT_CAINFO, [certPath UTF8String]);
+}
+
+- (void)__releaseCURLHandle:(void *)handle;
+{
+  CURL *curl = (CURL *)handle;
+  NSParameterAssert(curl);
+  curl_easy_cleanup(curl);
 }
 
 @end
