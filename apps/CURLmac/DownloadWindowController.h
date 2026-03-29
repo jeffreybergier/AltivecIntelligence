b7241dd63df4170e02b731dcac5d2227cf9ce13d
Jeffrey Bergier <jeffburg@jeffburg.com>
2026-03-29 11:11:26 +0900

    [AICC] Consolidated files into WindowController

--- apps/CURLmac/DownloadWindowController.h
+++ apps/CURLmac/DownloadWindowController.h
@@ -1,10 +1,67 @@
 #import <AppKit/AppKit.h>
+#import "CrossPlatform.h"
 
-@class DownloadView;
 @class DownloadManager;
 
-// The main window controller for the CURLmac application.
-// Manages the tabbed interface and coordinates downloads.
+// --- KeyValueTableView Interface ---
+@interface KeyValueTableView : NSView <XPTableViewDataSource, XPTableViewDelegate> {
+ @private
+  NSScrollView *_scrollView;
+  NSTableView *_tableView;
+  NSDictionary *_data;
+  NSArray *_sortedKeys;
+}
+
+- (void)setData:(NSDictionary *)data;
+- (NSDictionary *)data;
+
+@end
+
+// --- DownloadView Interface ---
+@interface DownloadView : NSView {
+ @private
+  NSTextField *urlField_;
+  NSButton *downloadButton_;
+  NSProgressIndicator *progressIndicator_;
+  NSImageView *imageView_;
+  NSTextField *statusLabel_;
+  NSString *identifier_;
+  DownloadManager *manager_;
+}
+
+- (NSTextField *)urlField;
+- (NSButton *)downloadButton;
+- (NSProgressIndicator *)progressIndicator;
+- (NSImageView *)imageView;
+- (NSTextField *)statusLabel;
+
+- (NSString *)identifier;
+- (void)setIdentifier:(NSString *)identifier;
+
+- (DownloadManager *)manager;
+- (void)setManager:(DownloadManager *)manager;
+
+@end
+
+// --- DownloadManager Interface ---
+@interface DownloadManager : NSResponder {
+ @private
+  DownloadView *view_;
+  NSMutableData *receivedData_;
+  Class connectionClass_;
+}
+
+- (id)initWithView:(DownloadView *)view 
+   connectionClass:(Class)connectionClass;
+
+- (void)downloadButtonClicked:(id)sender;
+
+- (DownloadView *)view;
+- (Class)connectionClass;
+
+@end
+
+// --- DownloadWindowController Interface ---
 @interface DownloadWindowController : NSWindowController {
  @private
   DownloadView *curlView_;
--- apps/CURLmac/DownloadWindowController.m
+++ apps/CURLmac/DownloadWindowController.m
@@ -1,12 +1,313 @@
 #import "DownloadWindowController.h"
-#import "CrossPlatform.h"
 #import <AICURLConnection.h>
-#import "DownloadView.h"
-#import "DownloadManager.h"
-#import "KeyValueTableView.h"
 
-@interface DownloadWindowController (Private)
+#pragma mark - KeyValueTableView Implementation
+
+@implementation KeyValueTableView
+
+- (id)initWithFrame:(NSRect)frame {
+  self = [super initWithFrame:frame];
+  if (self) {
+    [self setAutoresizingMask:NSViewWidthSizable | NSViewHeightSizable];
+
+    _scrollView = [[NSScrollView alloc] initWithFrame:[self bounds]];
+    [_scrollView setHasVerticalScroller:YES];
+    [_scrollView setHasHorizontalScroller:YES];
+    [_scrollView setAutoresizingMask:NSViewWidthSizable | NSViewHeightSizable];
+    [_scrollView setBorderType:NSBezelBorder];
+
+    _tableView = [[NSTableView alloc] initWithFrame:[[_scrollView contentView] bounds]];
+    
+    NSTableColumn *keyCol = [[NSTableColumn alloc] initWithIdentifier:@"Key"];
+    [[keyCol headerCell] setStringValue:@"Key"];
+    [keyCol setWidth:152.0];
+    [keyCol setEditable:NO];
+    [_tableView addTableColumn:keyCol];
+    [keyCol release];
+
+    NSTableColumn *valCol = [[NSTableColumn alloc] initWithIdentifier:@"Value"];
+    [[valCol headerCell] setStringValue:@"Value"];
+    [valCol setWidth:252.0];
+    [valCol setEditable:NO];
+    [valCol setResizingMask:NSTableColumnAutoresizingMask];
+    [_tableView addTableColumn:valCol];
+    [valCol release];
+
+    [_tableView setDataSource:self];
+    [_tableView setDelegate:self];
+    [_tableView setUsesAlternatingRowBackgroundColors:YES];
+    [_tableView setAutoresizingMask:NSViewWidthSizable];
+
+    [_scrollView setDocumentView:_tableView];
+    [self addSubview:_scrollView];
+  }
+  return self;
+}
+
+- (void)setData:(NSDictionary *)data {
+  if (_data != data) {
+    [_data release];
+    _data = [data retain];
+    
+    [_sortedKeys release];
+    _sortedKeys = [[[_data allKeys] sortedArrayUsingSelector:@selector(compare:)] retain];
+    
+    [_tableView reloadData];
+  }
+}
+
+- (NSDictionary *)data { return _data; }
+
+#pragma mark - NSTableDataSource
+
+- (NSInteger)numberOfRowsInTableView:(NSTableView *)tableView {
+  return (NSInteger)[_sortedKeys count];
+}
+
+- (id)tableView:(NSTableView *)tableView objectValueForTableColumn:(NSTableColumn *)tableColumn row:(NSInteger)row {
+  NSString *key = [_sortedKeys objectAtIndex:row];
+  if ([[tableColumn identifier] isEqualToString:@"Key"]) {
+    return key;
+  } else {
+    return [_data objectForKey:key];
+  }
+}
+
+- (void)dealloc {
+  [_scrollView release];
+  [_tableView release];
+  [_data release];
+  [_sortedKeys release];
+  [super dealloc];
+}
+
 @end
+
+#pragma mark - DownloadView Implementation
+
+@implementation DownloadView
+
+- (id)initWithFrame:(NSRect)frame;
+{
+  if ((self = [super initWithFrame:frame])) {
+    [self setAutoresizingMask:NSViewWidthSizable | NSViewHeightSizable];
+    
+    CGFloat height = frame.size.height;
+    CGFloat width = frame.size.width;
+    CGFloat padding = 8;
+    CGFloat buttonWidth = 90;
+
+    // 1. URL Field: Top row
+    urlField_ = [[NSTextField alloc] initWithFrame:NSMakeRect(padding, 
+                                                              height - 32, 
+                                                              width - 110, 
+                                                              24)];
+    [urlField_ setAutoresizingMask:NSViewWidthSizable | NSViewMinYMargin];
+    [[urlField_ cell] setPlaceholderString:@"Enter URL here..."];
+    [urlField_ setStringValue:@"https://platform.theverge.com/wp-content/uploads/sites/2/2026/03/Rank-Apple-Products-Lead-Art-1.jpg?quality=90&strip=all&crop=0%2C0%2C100%2C100&w=1440"];
+    [[urlField_ cell] setScrollable:YES];
+    [self addSubview:urlField_];
+    
+    // 2. Download Button: To the right of URL
+    downloadButton_ = [[NSButton alloc] initWithFrame:NSMakeRect(width - buttonWidth - padding, 
+                                                                  height - 36, 
+                                                                  buttonWidth, 
+                                                                  32)];
+    [downloadButton_ setTitle:@"Download"];
+    [downloadButton_ setBezelStyle:XPBezelStyleRounded];
+    [downloadButton_ setAutoresizingMask:NSViewMinXMargin | NSViewMinYMargin];
+    [downloadButton_ setTarget:self];
+    [downloadButton_ setAction:@selector(downloadButtonClicked:)];
+    [self addSubview:downloadButton_];
+
+    // 3. Progress Indicator: Below Top Row
+    progressIndicator_ = [[NSProgressIndicator alloc] initWithFrame:NSMakeRect(padding, 
+                                                                               height - 56, 
+                                                                               width - (padding * 2), 
+                                                                               16)];
+    [progressIndicator_ setStyle:XPProgressIndicatorStyleBar];
+    [progressIndicator_ setIndeterminate:NO];
+    [progressIndicator_ setMinValue:0.0];
+    [progressIndicator_ setMaxValue:1.0];
+    [progressIndicator_ setDoubleValue:1.0];
+    [progressIndicator_ setDisplayedWhenStopped:YES];
+    [progressIndicator_ setAutoresizingMask:NSViewWidthSizable | NSViewMinYMargin];
+    [self addSubview:progressIndicator_];
+
+    // 4. Status Label: At bottom
+    statusLabel_ = [[NSTextField alloc] initWithFrame:NSMakeRect(padding, 
+                                                                 4, 
+                                                                 width - (padding * 2), 
+                                                                 16)];
+    [statusLabel_ setStringValue:@"Ready"];
+    [statusLabel_ setBezeled:NO];
+    [statusLabel_ setDrawsBackground:NO];
+    [statusLabel_ setEditable:NO];
+    [statusLabel_ setSelectable:NO];
+    [statusLabel_ setFont:[NSFont systemFontOfSize:11]];
+    [statusLabel_ setAutoresizingMask:NSViewWidthSizable | NSViewMaxYMargin];
+    [self addSubview:statusLabel_];
+
+    // 5. Image View (Well): Centerpiece
+    imageView_ = [[NSImageView alloc] initWithFrame:NSMakeRect(padding, 
+                                                                24, 
+                                                                width - (padding * 2), 
+                                                                height - 84)];
+    [imageView_ setAutoresizingMask:NSViewWidthSizable | NSViewHeightSizable];
+    [imageView_ setImageFrameStyle:NSImageFrameGrayBezel];
+    [imageView_ setImageScaling:NSImageScaleProportionallyUpOrDown];
+    [self addSubview:imageView_];
+  }
+  return self;
+}
+
+- (void)dealloc;
+{
+  [urlField_ release];
+  [downloadButton_ release];
+  [progressIndicator_ release];
+  [statusLabel_ release];
+  [imageView_ release];
+  [identifier_ release];
+  [manager_ release];
+  [super dealloc];
+}
+
+- (void)downloadButtonClicked:(id)sender;
+{
+  if (manager_) [manager_ downloadButtonClicked:sender];
+}
+
+- (NSTextField *)urlField; { return urlField_; }
+- (NSButton *)downloadButton; { return downloadButton_; }
+- (NSProgressIndicator *)progressIndicator; { return progressIndicator_; }
+- (NSImageView *)imageView; { return imageView_; }
+- (NSTextField *)statusLabel; { return statusLabel_; }
+- (NSString *)identifier; { return identifier_; }
+- (void)setIdentifier:(NSString *)i; { [identifier_ autorelease]; identifier_ = [i copy]; }
+- (DownloadManager *)manager; { return manager_; }
+- (void)setManager:(DownloadManager *)m; { if (manager_ != m) { [manager_ release]; manager_ = [m retain]; } }
+
+@end
+
+#pragma mark - DownloadManager Implementation
+
+@interface DownloadManager (Private)
+- (void)updateStatus:(NSString *)text;
+@end
+
+@implementation DownloadManager
+
+- (id)initWithView:(DownloadView *)view 
+   connectionClass:(Class)connectionClass;
+{
+  if ((self = [super init])) {
+    view_ = [view retain];
+    connectionClass_ = connectionClass;
+  }
+  return self;
+}
+
+- (void)dealloc;
+{
+  [view_ release];
+  [receivedData_ release];
+  [super dealloc];
+}
+
+- (BOOL)acceptsFirstResponder; { return YES; }
+
+- (void)downloadButtonClicked:(id)sender;
+{
+  NSLog(@"[DownloadManager downloadButtonClicked:] class: %@", NSStringFromClass(connectionClass_));
+  NSString *urlStr = [[view_ urlField] stringValue];
+  NSURL *url = [NSURL URLWithString:urlStr];
+  if (!url) { [self updateStatus:@"Error: Invalid URL"]; return; }
+
+  [[view_ downloadButton] setEnabled:NO];
+  [[view_ progressIndicator] startAnimation:nil];
+  [[view_ imageView] setImage:nil];
+
+  [receivedData_ release];
+  receivedData_ = [[NSMutableData alloc] init];
+
+  NSURLRequest *request = [NSURLRequest requestWithURL:url];
+  [connectionClass_ connectionWithRequest:request delegate:self];
+}
+
+- (void)updateStatus:(NSString *)text; { [[view_ statusLabel] setStringValue:text]; }
+- (DownloadView *)view; { return view_; }
+- (Class)connectionClass; { return connectionClass_; }
+
+- (void)connection:(id)connection didReceiveResponse:(NSURLResponse *)response;
+{
+  if ([response isKindOfClass:[NSHTTPURLResponse class]]) {
+    NSInteger code = [(NSHTTPURLResponse *)response statusCode];
+    NSString *msg = [AIHTTPURLResponse localizedStringForStatusCode:code];
+    [self updateStatus:[NSString stringWithFormat:@"Response: %ld (%@)", (long)code, msg]];
+    
+    long long length = [response expectedContentLength];
+    if (length > 0) {
+      [[view_ progressIndicator] setIndeterminate:NO];
+      [[view_ progressIndicator] setMaxValue:(double)length];
+      [[view_ progressIndicator] setDoubleValue:0.0];
+    } else {
+      [[view_ progressIndicator] setIndeterminate:YES];
+    }
+
+    if (code < 200 || code > 299) {
+      [connection cancel];
+      NSString *statusText = [AIHTTPURLResponse localizedStringForStatusCode:code];
+      NSString *fullError = [NSString stringWithFormat:@"Failed: %ld (%@)", (long)code, statusText];
+      NSDictionary *userInfo = [NSDictionary dictionaryWithObject:fullError forKey:NSLocalizedDescriptionKey];
+      [self connection:connection didFailWithError:[NSError errorWithDomain:@"com.altivecintelligence.example" code:code userInfo:userInfo]];
+    }
+  }
+}
+
+- (void)connection:(id)connection didReceiveData:(NSData *)data;
+{
+  [receivedData_ appendData:data];
+  if (![[view_ progressIndicator] isIndeterminate]) {
+    [[view_ progressIndicator] incrementBy:(double)[data length]];
+  }
+  NSString *received = [NSString XP_stringFromByteCount:[receivedData_ length]];
+  if (![[view_ progressIndicator] isIndeterminate]) {
+    long long expected = (long long)[[view_ progressIndicator] maxValue];
+    NSString *total = [NSString XP_stringFromByteCount:expected];
+    [self updateStatus:[NSString stringWithFormat:@"Receiving: %@ / %@", received, total]];
+  } else {
+    [self updateStatus:[NSString stringWithFormat:@"Receiving: %@...", received]];
+  }
+}
+
+- (void)connection:(id)connection didFailWithError:(NSError *)error;
+{
+  NSLog(@"[DownloadManager connection:didFailWithError:] error: %@", error);
+  [[view_ progressIndicator] stopAnimation:nil];
+  [[view_ progressIndicator] setIndeterminate:NO];
+  [[view_ progressIndicator] setMaxValue:1.0];
+  [[view_ progressIndicator] setDoubleValue:1.0];
+  [[view_ downloadButton] setEnabled:YES];
+  [self updateStatus:[NSString stringWithFormat:@"Failed: %@", [error localizedDescription]]];
+  [self.nextResponder presentError:error];
+}
+
+- (void)connectionDidFinishLoading:(id)connection;
+{
+  NSLog(@"[DownloadManager connectionDidFinishLoading:] success!");
+  [[view_ progressIndicator] stopAnimation:nil];
+  [[view_ downloadButton] setEnabled:YES];
+  NSString *total = [NSString XP_stringFromByteCount:[receivedData_ length]];
+  [self updateStatus:[NSString stringWithFormat:@"Success! Total: %@", total]];
+  NSImage *image = [[NSImage alloc] initWithData:receivedData_];
+  if (image) { [[view_ imageView] setImage:image]; [image release]; }
+  else { [self updateStatus:@"Finished, but data is not a valid image."]; }
+}
+
+@end
+
+#pragma mark - DownloadWindowController Implementation
 
 @implementation DownloadWindowController
 
@@ -56,7 +357,6 @@
   curlView_ = [[DownloadView alloc] initWithFrame:[tabView contentRect]];
   [curlView_ setIdentifier:@"CURL"];
   
-  // Setup Manager for CURL - Inject AICURLConnection class
   curlManager_ = [[DownloadManager alloc] initWithView:curlView_ 
                                        connectionClass:[AICURLConnection class]];
   [curlView_ setManager:curlManager_];
@@ -72,7 +372,6 @@
   systemView_ = [[DownloadView alloc] initWithFrame:[tabView contentRect]];
   [systemView_ setIdentifier:@"System"];
   
-  // Setup Manager for System - Inject NSURLConnection class
   systemManager_ = [[DownloadManager alloc] initWithView:systemView_ 
                                           connectionClass:[NSURLConnection class]];
   [systemView_ setManager:systemManager_];
