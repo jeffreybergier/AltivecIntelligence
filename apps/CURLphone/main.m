143592653abddde0e238ae5f6c0532055ccaebf9
Jeffrey Bergier <jeffburg@jeffburg.com>
2026-04-01 10:11:04 +0000

    [AICC] consolidated files in CURLphone app

--- apps/CURLphone/main.m
+++ apps/CURLphone/main.m
@@ -5,7 +5,73 @@
 
 #import <UIKit/UIKit.h>
 #import "DownloadViewController.h"
-#import "KeyValueTableViewController.h"
+#import "CrossPlatform.h"
+#import <AICURLConnection.h>
+
+// --- KeyValueTableViewController Interface ---
+// A table view controller that displays the versions of the linked 
+// libraries (libcurl, openssl, etc.) in a grouped style.
+@interface KeyValueTableViewController : UITableViewController {
+ @private
+  NSDictionary *versions_;
+  NSArray *sortedKeys_;
+}
+@end
+
+// --- KeyValueTableViewController Implementation ---
+@implementation KeyValueTableViewController
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  [[self navigationItem] setTitle:@"Linked Libraries"];
+  
+  // Fetch versions from AICURLConnection
+  versions_ = [[NSDictionary alloc] initWithObjectsAndKeys:
+    [AICURLConnection zlibVersion], @"libz",
+    [AICURLConnection sslVersion], @"libssl",
+    [AICURLConnection curlVersion], @"libcurl",
+    [AICURLConnection cryptoVersion], @"libcrypto",
+    nil];
+  
+  sortedKeys_ = [[[versions_ allKeys] 
+    sortedArrayUsingSelector:@selector(compare:)] retain];
+}
+
+- (void)dealloc {
+  [versions_ release];
+  [sortedKeys_ release];
+  [super dealloc];
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+  return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView 
+    numberOfRowsInSection:(NSInteger)section {
+  return [sortedKeys_ count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView 
+         cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+  static NSString *CellIdentifier = @"InfoCell";
+  UITableViewCell *cell = [tableView 
+    dequeueReusableCellWithIdentifier:CellIdentifier];
+  if (cell == nil) {
+    cell = [[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 
+                                   reuseIdentifier:CellIdentifier] autorelease];
+  }
+  
+  NSString *key = [sortedKeys_ objectAtIndex:[indexPath row]];
+  [[cell textLabel] setText:key];
+  [[cell detailTextLabel] setText:[versions_ objectForKey:key]];
+  
+  return cell;
+}
+
+@end
 
 // --- AppDelegate Interface ---
 @interface AppDelegate : UIResponder <UIApplicationDelegate> {
